9f7d88fac84769c5490a1826a11f8b3bd5ea8f77
Maxim <mailtime2010@gmail.com>
2020-09-11 16:52:37 +0300

    travis-heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/boot-1.0-SNAPSHOT.jar
--- src/main/java/com/boot/Application.java
+++ src/main/java/com/boot/Application.java
@@ -1,7 +1,15 @@
 package com.boot;
 
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
 
 /**
  * Main.
@@ -11,8 +19,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 9/2/2020
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected final SpringApplicationBuilder configure(
+            final SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    @Bean
+    public SpringLiquibase liquibase(@Qualifier("dataSource") final DataSource ds) {
+        final SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
+        liquibase.setDataSource(ds);
+        return liquibase;
     }
 }
